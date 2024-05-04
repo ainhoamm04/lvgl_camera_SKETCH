b53eb01cd20085444e6bc768da835b050b5c7a0e
ainhoamm04 <amm00384@red.ujaen.es>
2024-05-04 18:12:21 +0200

    Creo repositorio

---
+++ src/camera.h
@@ -0,0 +1,16 @@
+#ifndef __CAMERA_H
+#define __CAMERA_H
+
+#include "Arduino.h"
+//#define CAMERA_MODEL_ESP32S3_EYE
+//#define CAMERA_MODEL_WROVER_KIT
+#include "camera_pins.h"
+#include "esp_camera.h"
+
+int camera_init();//Initialize the camera drive
+void camera_set_flip_vertical(bool state);//Flip Vertical
+void camera_set_mirror_horizontal(bool state);//Mirror Horizontal
+bool camera_get_flip_vertical(void);
+bool camera_get_mirror_horizontal(void);
+
+#endif
